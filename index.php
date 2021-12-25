423151da9a6b5285e2820ca8934221fa0d1e03f5
云默安 <40316661+Yunmoan@users.noreply.github.com>
2021-12-25 22:34:35 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,161 @@
+<!--
+███████╗ ██████╗ ██╗████████╗    ███╗   ██╗███████╗████████╗██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗
+╚══███╔╝██╔════╝ ██║╚══██╔══╝    ████╗  ██║██╔════╝╚══██╔══╝██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝
+  ███╔╝ ██║  ███╗██║   ██║       ██╔██╗ ██║█████╗     ██║   ██║ █╗ ██║██║   ██║██████╔╝█████╔╝ 
+ ███╔╝  ██║   ██║██║   ██║       ██║╚██╗██║██╔══╝     ██║   ██║███╗██║██║   ██║██╔══██╗██╔═██╗ 
+███████╗╚██████╔╝██║   ██║       ██║ ╚████║███████╗   ██║   ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗
+╚══════╝ ╚═════╝ ╚═╝   ╚═╝       ╚═╝  ╚═══╝╚══════╝   ╚═╝    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ 
+——————版权所有 © 2017-2022 ZGIT Network & ZGIT.保留所有权利—————非法盗用将会受到法律追究！————
+-->
+<!DOCTYPE html>
+
+<html lang="zh-CN">
+
+<head>
+    <meta name="baidu-site-verification" content="code-T3SmGL0Gr8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" />
+    <meta name="renderer" content="webkit" />
+    <meta name=“Keywords” content="至远光辉信息技术,至远光辉,ZGIT,ZGIT Network,RCNBBS,云启之域,OpenFrp,工作室,媒体" />
+    <meta name="description" content="至远光辉信息技术 OpenFrp、云启之域论坛、MC云启之域群组服务器运营者。">
+    <link rel="icon" type="image/x-icon" href="https://img.zyghit.cn/img/zgit.ico">
+    <meta name="force-rendering" content="webkit" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7078862192604752"
+        crossorigin="anonymous"></script>
+    <!-- Copyright © 2017-2021 ZGIT Network,ZGIT.All rights reserved。 -->
+    <meta charset="utf-8">
+    <title>ZGIT在线工具箱 | Online Tools</title>
+    <script type="text/javascript">
+        window.onload = function () {
+            console.log("███████╗ ██████╗ ██╗████████╗\n╚══███╔╝██╔════╝ ██║╚══██╔══╝\n  ███╔╝ ██║  ███╗██║   ██║   \n ███╔╝  ██║   ██║██║   ██║   \n███████╗╚██████╔╝██║   ██║  \n ╚══════╝ ╚═════╝ ╚═╝   ╚═╝   \n                             ");
+            console.log("我们虽是小工作室，但我们欢迎你的到来！\n如果你有一定的技术，你想随我们一起奋斗，那就请加入我们吧！\n\n");
+            console.log("至远·光辉信息技术 联系方式: campus@zyghit.cn");
+            console.log("Copyright © 2017-2022 ZGIT Network & ZGIT.All rights reserved.");
+        }
+
+    </script>
+    <style>
+        body {
+            /*https://img.zyghit.cn/img/93719055_p0.jpg*/
+            background-color: #000;
+            background-image: url();
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+        }
+    </style>
+    <!-- MDUI CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/css/mdui.min.css"
+        integrity="sha384-cLRrMq39HOZdvE0j6yBojO4+1PrHfB7a9l5qLcmRm/fiWXYY+CndJPmyu5FV/9Tw" crossorigin="anonymous" />
+
+    <!-- MDUI JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/js/mdui.min.js"
+        integrity="sha384-gCMZcshYKOGRX9r6wbDrvF+TcCCswSHFucUzUPwka+Gr+uHgjlYvkABr95TCOz3A"
+        crossorigin="anonymous"></script>
+</head>
+
+<body class="mdui-theme-primary-blue-800">
+    <!-- 顶部栏 -->
+    <div class="mdui-appbar">
+        <div class="mdui-toolbar mdui-color-blue-800 mdui-shadow-8">
+            <a href="/" class="mdui-typo-headline"> ZGIT Network</a>
+            <small>选择您的语言 - Select your Language</small>
+            <div class="mdui-toolbar-spacer"></div>
+            <a href="#" mdui-tooltip="{content: '语言'}"><i class="mdui-icon material-icons">language</i></a>
+        </div>
+    </div><br>
+    <div class="mdui-container pjax-container" id="main">
+        <div id="topic" class="mdui-m-b-1" style="margin-bottom: 10px;">
+            <div class="mdui-panel mdui-shadow-7" mdui-panel>
+            </div>
+        </div>
+        <br>
+        <div class="mdui-row">
+            <!-- 1 -->
+            <div class="mdui-col-sm-4 mdui-col-md-3">
+                <div class="mdui-card mdui-shadow-7">
+                    <div class="mdui-card-media">
+                        <img src="https://img.zyghit.cn/img/ZGITNETWORK.png"
+                            onload='if (this.width>140 || this.height>226) if (this.width/this.height>140/226) this.width=140; else this.height=226;' />
+                    </div>
+                    <!-- 卡片标题和副标题 -->
+                    <div class="mdui-card-primary">
+                        <div class="mdui-card-primary-title">ZGIT Network</div>
+                        <div class="mdui-card-primary-subtitle">ZGIT Network 是至远光辉信息技术的网络服务平台</div>
+                    </div>
+                    <!-- 卡片内容 -->
+                    <div class="mdui-card-content"></div>
+                </div><br>
+                <div class="mdui-card mdui-shadow-7">
+                    <!-- 卡片内容 -->
+                    <div class="mdui-card-content">
+                        <div class="mdui-text-color-black-secondary">Copyright &copy; 2017-2022 ZGIT Network.All rights
+                            reserved.<br>
+                            &copy; 至远光辉信息技术 保留所有权利。<br>
+                            光辉主页 Version: Alpha 1.1<br><br>
+                            背景图片来自网络
+                            <!--PIXIV, 画师: <b>周憂</b>--><br>
+                            本站使用了 MDUI，基于MDUI设计语言编写<br>
+                            MDUI 是开源项目，任何人均可使用
+                        </div>
+                    </div>
+                </div><br>
+            </div>
+            <!-- 2 -->
+            <div class="mdui-col-sm-12 mdui-col-md-9">
+                <div class="mdui-card mdui-shadow-7">
+                    <div class="mdui-card-primary">
+
+                        <div class="mdui-card-primary-title">选择您的语言 | Select Your Language</div>
+                        <div class="mdui-card-primary-subtitle">在此处选择ZGIT Toolbox所支持的语言 | Select the Language supported by ZGIT Toolbox here</div>
+                    </div>
+                    <!-- 卡片内容 -->
+                    <div class="mdui-card-content">
+                        <div class="mdui-text-color-black-secondary">
+                            <div class="mdui-list">
+                                <a href="/tools/zh-CN/" class="mdui-list-item mdui-ripple">中文(简体) - Simplified Chinese</a>
+                                <a href="/tools/zh-TW/" class="mdui-list-item mdui-ripple">中文(繁体) - Traditional Chinese</a>
+                                <a href="/tools/en/" class="mdui-list-item mdui-ripple">英语 - English</a>
+                                <a href="/tools/jp/" class="mdui-list-item mdui-ripple">日语 - Japanese</a>
+                                <a href="/tools/de/" class="mdui-list-item mdui-ripple">德语 - German</a>
+                                <a href="/tools/fr/" class="mdui-list-item mdui-ripple">法语 - French</a>
+                                <a href="/tools/ru/" class="mdui-list-item mdui-ripple">俄语 - Russian</a>
+                                <p>更多语言正在支持... | More languages are supporting...</p>
+                                <hr>
+                                <p><span>很遗憾的是，我们现在还不支持保存您选择的语言，您可能需要每次登录都选择一次，不过您可以通过收藏相应地址链接的方式来达到保存语言选择的目的。<br>Unfortunately, we don't support saving the language you selected yet. You may need to select it every time you log in, but you can save the language selection by collecting the corresponding address links.</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div><br>
+            </div>
+        </div>
+    </div>
+    <script src="js/mdui.min.js"></script>
+    <script>
+        var $ = mdui.$;
+        function copyToClipboard(s) {
+            mdui.snackbar({
+                message: '文本已复制',
+                position: 'right-top',
+            });
+            if (window.clipboardData) {
+                window.clipboardData.setData('text', s);
+            } else {
+                (function (s) {
+                    document.oncopy = function (e) {
+                        e.clipboardData.setData('text', s);
+                        e.preventDefault();
+                        document.oncopy = null;
+                    }
+                })(s);
+                document.execCommand('Copy');
+            }
+        }
+    </script>
+</body>
+
+</html>
---
+++ jp/index.php
@@ -0,0 +1,172 @@
+<!--
+███████╗ ██████╗ ██╗████████╗    ███╗   ██╗███████╗████████╗██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗
+╚══███╔╝██╔════╝ ██║╚══██╔══╝    ████╗  ██║██╔════╝╚══██╔══╝██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝
+  ███╔╝ ██║  ███╗██║   ██║       ██╔██╗ ██║█████╗     ██║   ██║ █╗ ██║██║   ██║██████╔╝█████╔╝ 
+ ███╔╝  ██║   ██║██║   ██║       ██║╚██╗██║██╔══╝     ██║   ██║███╗██║██║   ██║██╔══██╗██╔═██╗ 
+███████╗╚██████╔╝██║   ██║       ██║ ╚████║███████╗   ██║   ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗
+╚══════╝ ╚═════╝ ╚═╝   ╚═╝       ╚═╝  ╚═══╝╚══════╝   ╚═╝    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ 
+——————版权所有 © 2017-2022 ZGIT Network & ZGIT.保留所有权利—————非法盗用将会受到法律追究！————
+-->
+<!DOCTYPE html>
+
+<html lang="ja-JP">
+<head>
+    <meta name="baidu-site-verification" content="code-T3SmGL0Gr8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"/>
+    <meta name="renderer" content="webkit"/>
+    <meta name=“Keywords” content="至远光辉信息技术,至远光辉,ZGIT,ZGIT Network,RCNBBS,云启之域,OpenFrp,工作室,媒体" />
+    <meta name="description" content="至远光辉信息技术 OpenFrp、云启之域论坛、MC云启之域群组服务器运营者。">
+     <link rel="icon" type="image/x-icon" href="https://img.zyghit.cn/img/zgit.ico">
+    <meta name="force-rendering" content="webkit"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7078862192604752"
+     crossorigin="anonymous"></script>
+    <!-- Copyright © 2017-2021 ZGIT Network,ZGIT.All rights reserved。 -->
+<meta charset="utf-8">
+<title>ZGITオンラインツールボックス</title>
+<script type="text/javascript">
+            window.onload = function(){
+            console.log("███████╗ ██████╗ ██╗████████╗\n╚══███╔╝██╔════╝ ██║╚══██╔══╝\n  ███╔╝ ██║  ███╗██║   ██║   \n ███╔╝  ██║   ██║██║   ██║   \n███████╗╚██████╔╝██║   ██║  \n ╚══════╝ ╚═════╝ ╚═╝   ╚═╝   \n                             ");
+            console.log("我们虽是小工作室，但我们欢迎你的到来！\n如果你有一定的技术，你想随我们一起奋斗，那就请加入我们吧！\n\n");
+            console.log("至远·光辉信息技术 联系方式: campus@zyghit.cn");
+            console.log("Copyright © 2017-2022 ZGIT Network & ZGIT.All rights reserved.");
+            }
+ 
+</script>
+<style>
+body{
+    /*https://img.zyghit.cn/img/93719055_p0.jpg*/
+   background-color: #000;
+	background-image: url(https://api.r10086.com/%E5%8A%A8%E6%BC%AB%E7%BB%BC%E5%90%882.php?);
+	background-size: cover;
+	background-position: center;
+	background-attachment: fixed;
+	background-repeat: no-repeat;
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+}
+</style>
+<!-- MDUI CSS -->
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/css/mdui.min.css"
+  integrity="sha384-cLRrMq39HOZdvE0j6yBojO4+1PrHfB7a9l5qLcmRm/fiWXYY+CndJPmyu5FV/9Tw"
+  crossorigin="anonymous"
+/>
+
+<!-- MDUI JavaScript -->
+<script
+  src="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/js/mdui.min.js"
+  integrity="sha384-gCMZcshYKOGRX9r6wbDrvF+TcCCswSHFucUzUPwka+Gr+uHgjlYvkABr95TCOz3A"
+  crossorigin="anonymous"
+></script>
+</head>
+<body class="mdui-theme-primary-blue-800">
+    <!-- 顶部栏 -->
+    <div class="mdui-appbar">
+  <div class="mdui-toolbar mdui-color-blue-800 mdui-shadow-8">
+    <a href="/" class="mdui-typo-headline"> ZGIT Network</a>
+    <small>オンラインツールボックス</small>
+    <div class="mdui-toolbar-spacer"></div>
+    <a href="/tools/" class="mdui-btn mdui-btn-icon" mdui-tooltip="{content:'ZGITオンラインツールボックス'}">
+    <i class="mdui-icon material-icons">business_center</i></a>
+    <a href="/tools/" mdui-tooltip="{content: '言語'}"><i class="mdui-icon material-icons">language</i></a>
+  </div>
+</div><br>
+<div class="mdui-container pjax-container" id="main">
+      <div id="topic" class="mdui-m-b-1" style="margin-bottom: 10px;">
+        <div class="mdui-panel mdui-shadow-7" mdui-panel>
+	        </div>
+	    </div>
+	<br>
+    <div class="mdui-row">
+        <!-- 1 -->
+        <div class="mdui-col-sm-4 mdui-col-md-3">
+            <div class="mdui-card mdui-shadow-7">
+                <div class="mdui-card-media">
+                    <img src="https://img.zyghit.cn/img/ZGITNETWORK.png" onload='if (this.width>140 || this.height>226) if (this.width/this.height>140/226) this.width=140; else this.height=226;'/>
+                </div>
+                <!-- 卡片标题和副标题 -->
+                <div class="mdui-card-primary">
+                    <div class="mdui-card-primary-title">ZGIT Network</div>
+                    <div class="mdui-card-primary-subtitle">ZGIT Networkは至遠光輝情報技術までのネットワークサービスプラットフォームです。</div>
+                </div>
+                <!-- 卡片内容 -->
+                <div class="mdui-card-content"></div>
+            </div><br>
+            <div class="mdui-card mdui-shadow-7">
+                <!-- 卡片内容 -->
+                <div class="mdui-card-content">
+                    <div class="mdui-text-color-black-secondary">Copyright &copy; 2017-2022 ZGIT Network.All rights reserved.<br>
+                    &copy; 至遠光輝情報技術 すべての権利を保持。<br>
+                    光輝ホームページ Version: Alpha 1.1<br><br>
+                    背景画像はネットワークから<!--PIXIV, 画师: <b>周憂</b>--><br>
+                    当駅はMDUIを使用して、MDUIに基づいて言語を設計して書きます<br>
+                    MDUIはオープンソースプロジェクトで、誰でも利用できます</div>
+                </div>
+            </div><br>
+        </div>
+        <!-- 2 -->
+	    <div class="mdui-col-sm-12 mdui-col-md-9">
+	        <div class="mdui-card mdui-shadow-7">
+                <!-- 卡片内容 -->
+                <div class="mdui-card-content">
+                    <div class="mdui-text-color-black-secondary">123</div>
+                </div>
+            </div><br>
+	    </div>
+    </div>
+  </div>
+  <script src="js/mdui.min.js"></script>
+  <!--<script type="text/javascript"> 
+        window.onload=function(){
+            var go_time=new Date("2021/11/7 22:00:00");
+            //  countdown：倒计时
+                 function  countdown(){
+                var now_time=new Date();
+                var alltime =go_time.getTime() -now_time.getTime ();  //总的时间（毫秒）
+               var haoscend =alltime%1000;  //毫秒
+                //console.log(haoscend);
+                var scend = parseInt ((alltime/1000)%60  ) ;  //秒
+                //console.log(scend);
+                var minute =parseInt((alltime/1000/60)%60  ) ;  //  分钟
+               // console.log(minute);
+                var hour =parseInt((alltime/1000/60/60)%24 ) ;   //小时
+               // console.log(hour);
+                var day=parseInt((alltime/1000/60/60/24)%30);   //天数
+               // console.log(day);
+                var month=parseInt((alltime/1000/60/60/24/30)%12); //月
+               // console.log(month);
+                var btime=document.getElementById("block");
+                var time1=document.getElementById("shi_jian");
+                time1.innerHTML = (day+"天"+hour+"时"+minute +"分"+scend +"秒" );
+                setTimeout (countdown ,1);  //不加括号
+            }
+           countdown ();
+        }
+
+    </script>-->
+  <script> 
+        var $ = mdui.$;
+        function copyToClipboard(s) {
+            mdui.snackbar({
+            message: '文本已复制',
+            position: 'right-top',
+            });
+            if (window.clipboardData) {
+                window.clipboardData.setData('text', s);
+            } else {
+                (function(s) {
+                    document.oncopy = function(e) {
+                        e.clipboardData.setData('text', s);
+                        e.preventDefault();
+                        document.oncopy = null;
+                    }
+                })(s);
+                document.execCommand('Copy');
+            }
+        }
+    </script>
+</body>
+</html>
